b9f49e18a34e7be8d6bf9fa3b90bf9d6f855497c
cyy89259 <cyy89259@gmail.com>
2016-03-07 17:12:07 +0800

    hw
    123

---
+++ 0307.R
@@ -0,0 +1,46 @@
+a<-5
+b<-3
+a+b
+x<-c(1,2,3)
+x+x
+y<-c(1,2,3,4)
+x+y
+z<-c(1,2,3,4,5,6)
+x+z
+x1<-c(1,2,3)
+x+x1
+sqrt(100)
+sin(90)
+sin(60)
+round(2266678555,-9)
+
+class(a)
+is.character(a)
+longx<-1:200
+length(longx)
+stringx<-"How are you?"
+length(stringx)
+nchar(stringx)
+seq(1,9,2)
+1:50
+1.4:5.6
+x<-1:5
+x+3
+y<-6:10
+x+y
+rep(1:5,3)
+
+x<-1:30
+sum(x)
+mean(x)
+max(x)
+min(x)
+median(x)
+round(sd(x),digits = 3)
+summary(x)
+sort(x)
+sort(x,decreasing = T)
+rev(x)
+matrix(nrow=2,ncol=3)
+matrix(1:6,nrow=2,ncol=3)
+dim(matrix(1:6,nrow=2,ncol=3))
